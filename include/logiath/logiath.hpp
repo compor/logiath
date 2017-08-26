dc05764f6abce82cf386d26cbf993ae06b427cad
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-26 18:47:08 +0100

    cleanup and initial intention of design

--- include/logiath/logiath.hpp
+++ include/logiath/logiath.hpp
@@ -13,52 +13,51 @@
 // using std::cout
 // using std::endl
 
-enum class log_severity : unsigned int { INFO, DEBUG };
+namespace logiath {
 
-template <const std::string &prefix>
-class logger {
- public:
-  template <log_severity severity, typename... Ts>
-  void print(Ts... args) {
-    switch (severity) {
-      case (log_severity::INFO):
-        std::cout << "[INFO] ";
-        break;
-      case (log_severity::DEBUG):
-        std::cout << "[DEBUG] ";
-        break;
-      default:
-        std::cout << "[UNKNOWN] ";
-        break;
-    }
+namespace detail {
 
-    std::cout << prefix << ": ";
+void print_impl() { std::cout << std::endl; }
 
-    print_impl(args...);
+template <typename T, typename... Ts>
+void print_impl(T v, Ts... args) {
+  std::cout << v;
+  print_impl(args...);
+}
 
-    return;
-  }
+}  // namespace detail end
 
- private:
-  void print_impl() {
-    std::cout << std::endl;
-    return;
-  }
+enum class severity_t : unsigned int {
+  EMERG,
+  ALERT,
+  CRIT,
+  ERR,
+  WARN,
+  NOTICE,
+  INFO,
+  DEBUG
+};
 
-  template <typename T, typename... Ts>
-  void print_impl(T v, Ts... args) {
-    std::cout << v;
+template <severity_t severity>
+struct Severity {
+  Severity() = delete;
+  Severity(const Severity &) = delete;
+
+  const std::string &operator() const { return std::string{""}; }
+
+  severity_t value = severity;
+};
+
+template <typename Output, typename SeverityFilter, typename Prefix>
+struct logiath {
+  template <typename... Ts>
+  void log(Ts... args) {
     print_impl(args...);
 
     return;
   }
 };
 
-extern const std::string log_prefix{"LOGIATH"};
-
-static logger<log_prefix> logiath;
-
-#define LOG_INFO logiath.print<log_severity::INFO>
-#define LOG_DEBUG logiath.print<log_severity::DEBUG>
+}  // namespace logiath end
 
 #endif  // header
