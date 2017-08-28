8e059d04d5788b15e3b10fc881c6fd9b07785686
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-28 18:26:56 +0100

    add example utilities

--- examples/util.hpp
+++ examples/util.hpp
@@ -5,6 +5,18 @@
 #ifndef LOGIATH_EXAMPLE_UTIL_HPP
 #define LOGIATH_EXAMPLE_UTIL_HPP
 
+#include <string>
+// using std::string
+
+#include <iostream>
+// using std::cerr
+
+#include <sstream>
+// using std::stringstream
+
+#include <utility>
+// using std::forward
+
 namespace example {
 
 struct CerrOutput {
@@ -12,9 +24,24 @@ struct CerrOutput {
   static void close() {}
 
   template <typename T>
-  static void print(const T &v) {
+  static void print(T &&v) {
     std::cerr << v;
   }
+};
+
+struct StringStreamOutput {
+  static void open() {}
+  static void close() {}
+
+  template <typename T>
+  void print(T &&v) {
+    ss << v;
+  }
+
+  std::string to_string() const { return ss.str(); }
+
+ protected:
+  std::stringstream ss;
 };
 
 }  // namespace example end
