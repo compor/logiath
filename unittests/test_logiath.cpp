cca18d9a335c239f8388300b551d0c0a77c184eb
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-28 18:54:33 +0100

    add unit test cases

--- unittests/test_logiath.cpp
+++ unittests/test_logiath.cpp
@@ -33,4 +33,27 @@ TEST_F(test_logiath, alert_filter_alert_message) {
   EXPECT_TRUE(!cmp);
 }
 
+TEST_F(test_logiath, alert_filter_debug_message) {
+  Logiath<example::StringStreamOutput, SeverityFilter<severity::ALERT> > logger;
+
+  std::string msg{"message 1"};
+  logger.log(DEBUG, msg);
+  msg += "\n";
+
+  auto cmp = msg.compare(logger.to_string());
+
+  EXPECT_TRUE(!cmp);
+}
+
+TEST_F(test_logiath, alert_filter_emerg_message) {
+  Logiath<example::StringStreamOutput, SeverityFilter<severity::ALERT> > logger;
+
+  std::string empty{""};
+  logger.log(EMERG, "message 1");
+
+  auto cmp = empty.compare(logger.to_string());
+
+  EXPECT_TRUE(!cmp);
+}
+
 }  // namespace anonymous end
